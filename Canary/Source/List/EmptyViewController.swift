cd049a3c7ece9584b42b323ea8885ee00e9553ec
Alex McMillan <alex.mcmillan@fanduel.com>
2018-09-10 18:33:00 +0100

    More localization

--- Canary/Source/List/EmptyViewController.swift
+++ Canary/Source/List/EmptyViewController.swift
@@ -1,4 +1,11 @@
 import UIKit
 
 class EmptyViewController: UIViewController {
+    
+    @IBOutlet weak private var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = NSLocalizedString("Dream something up", comment: "")
+    }
 }
